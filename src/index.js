2a969675926cd9879d72cb8d8421f8de20106778
Luis Auz <luisauz.geek@gmail.com>
2019-11-05 23:29:31 -0500

    Conexion app web con base de datos
    Se realiza la creacion de dos archivos para la conexion de base de datos
    de la app web (database.js & keys.js).
    
    keys.js contiene la direccion url donde apunta la conexion de la base de datos.
    
    database.js contiene la configuracion de la conexion, manteniendo un
    enlace (objeto) con el archivo keys.js para reconocer el sitio donde
    encontrar la conexion.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const config = require('./server/config');
 
+// database
+require('./database');
+
 const app = config(express());
 
+// starting the server 
 app.listen(app.get('port'), ()=>{
     console.log('Server on port', app.get('port'));
 });
---
+++ src/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    
+    database: {
+        URI: 'mongodb://localhost/auz_social_network'
+    }
+    /*AQUI PUEDO INTEGRAR LAS DIFERENTES CONEXIONES:
+    Por ejemplo, integracion del servicio de redes sociales como 
+    FACEBOOK & TWITTER*/
+}
