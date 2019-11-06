e8b02ca9ad7ee9e5c129e5c12cfd260bc148b4b8
Luis Auz <luisauz.geek@gmail.com>
2019-11-06 11:38:27 -0500

    Configuracion del motor de plantillas handlebars.
    La explicacion del codigo nuevo en config.js se encuentra comentada
    dentro del mismo archivo.

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,27 @@
+const path = require('path'); //path permite unir directorios con join 
+const exphbs = require('express-handlebars'); //motor de plantillas
+
 module.exports = app =>{
     
-    //Settings
-    app.set('port', process.env.PORT || 3000);
+    // settings
+    app.set('port', process.env.PORT || 3000); //configuracion del puerto
+    app.set('views', path.join(__dirname, 'views')); //definir donde esta la carpeta de las vistas 
+    //configuracion de motor de plantilla - handlebars
+    app.engine('.hbs', exphbs({ // '.hbs' es '.handlebars'
+        defaultLayout: 'main', //marco principal
+        // indicar lugares de partials & layouts
+        partialsDir: path.join(app.get('views'), 'partials'), // los partials son pedazos de codigo html reutilizables en cualquier parte del sitio web
+        layoutsDir: path.join(app.get('views'), 'layouts'), //layouts almacena marcos o plantillas reutilizables
+        extname: '.hbs', //extension de los archivos
+        helpers: require('./helpers') //funciones reutilizables dentro de hanlebars. Se crea el archivo helpers.js y se ubica aqui!
+    }));
+    app.set('view engine', '.hbs'); //utilizar el motor de plantilla
+
+    // middlewares
+
+    // routes
+
+    //errorhandlers
     
     return app;
 }
