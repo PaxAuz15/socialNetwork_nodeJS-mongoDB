0f90d1a44352bea69a82b1c10c20d67936c54a9d
Luis Auz <luisauz.geek@gmail.com>
2019-11-17 17:17:52 -0500

    Se agrega la conexion de las imagenes con el projecto para poder usarlo en las vistas

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,10 @@
 const ctrl = {};
 
-ctrl.index = (req, res) =>{
-    res.render('index');
+const { Image } = require('../models');
+
+ctrl.index = async (req, res) =>{
+    const images = await Image.find().sort({ timestamp: -1 });
+    res.render('index', {images});
 };
 
 module.exports = ctrl;
