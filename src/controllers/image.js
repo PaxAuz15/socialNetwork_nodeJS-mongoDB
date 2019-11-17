d7d09e36a04cad9fa1a107aa7dc2808cbdf28dc8
Luis Auz <luisauz.geek@gmail.com>
2019-11-16 22:27:20 -0500

    Agregando metodo para guardar las imagenes dentro de Mongo.
    Todo se esta realizando a nivel local. Aun no se agrega el Mongo Atlas.

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -2,26 +2,53 @@ const path = require('path');
 const {randomNumber} = require('../helpers/libs');
 const fs = require('fs-extra');
 
+const {Image} = require('../models');
+
 const ctrl = {};
 
 ctrl.index = (req, res) => {
 
 };
 
-ctrl.create = async(req, res) => {
-    const imgUrl = randomNumber();
-    console.log(imgUrl);
-    const imageTempPath = req.file.path;
-    const ext = path.extname(req.file.originalname).toLowerCase();
-    //const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`)
-    const targetPath = path.resolve('src/public/upload/'+imgUrl+' '+ ext)
+ctrl.create = (req, res) => {
+    
+    const saveImage = async() => {
+        const imgUrl = randomNumber();
+
+        const images = await Image.find({filename: imgUrl});
+        if (images.length > 0){
+            saveImage();
+        } else{
+            console.log(imgUrl);
+            const imageTempPath = req.file.path;
+            const ext = path.extname(req.file.originalname).toLowerCase();
+            //const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`)
+            const targetPath = path.resolve('src/public/upload/'+imgUrl+' '+ ext)
 
 
-    if(ext === '.png' || ext === '.jpg' || ext === '.jepg' || ext === '.gif'){
-        await fs.rename(imageTempPath, targetPath);
-    }
+            if(ext === '.png' || ext === '.jpg' || ext === '.jepg' || ext === '.gif'){
+                await fs.rename(imageTempPath, targetPath);
+                const newImg = new Image({
+                    tittle: req.body.tittle,
+                    filename: imgUrl + ext,
+                    description: req.body.description
+                });
+                const imageSaved = await newImg.save();
+                res.redirect('/images');
+                //console.log(newImg)
+            } else{
+                await fs.unlink(imageTempPath);
+                res.status(500).json({error: 'Only Images are allowed'});
+            }
 
-    res.send('works!');
+        }
+
+        
+    };
+
+    saveImage();
+    
+    
 };
 
 ctrl.like = (req, res) => {
